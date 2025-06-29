6a4ce219d8eccc27a14118bdc6929915d5df75b4
Andrei <andreimandrique3@gmail.com>
2025-06-29 11:16:49 +0800

    add basic socket connection and room

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const PORT = 3000;
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected ${socket.id}`);
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on("leaveRoom", (roomId) => {
+    socket.leave(roomId);
+    console.log(`${socket.id} left room ${roomId}`);
+  });
+
+  socket.on("messageRoom", ({ roomId, message }) => {
+    console.log(`${roomId} ${message}`);
+    io.to(roomId).emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => console.log(`user disconnect ${socket.id}`));
+});
 
 app.use(express.json());
 
@@ -13,6 +49,6 @@ app.get("/", indexRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
